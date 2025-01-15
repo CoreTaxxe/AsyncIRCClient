8bf3b82e8b8896d6f50b30808b8cbbc20b52ffd2
CoreTaxxe <47256718+CoreTaxxe@users.noreply.github.com>
2025-01-15 01:03:55 +0100

    Github & pypy issue.

--- async_irc_client/async_irc_client.py
+++ async_irc_client/async_irc_client.py
@@ -1,5 +1,5 @@
 __author__ = "coretaxxe"
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 
 import asyncio
 import datetime
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='AsyncIRCClient',
-    version='1.0.2',
+    version='1.0.3',
     description='Async IRC Client',
     long_description=long_description,
     long_description_content_type='text/markdown',
